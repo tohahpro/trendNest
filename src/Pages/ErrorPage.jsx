b7a65ebafee6f1acd71bd8a084f14eb879047070
Toha810 <tohahpro@gmail.com>
2023-11-30 10:41:37 +0600

    error page setup

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,4 +1,5 @@
 import { Helmet } from "react-helmet-async";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
     return (
@@ -7,6 +8,15 @@ const ErrorPage = () => {
                 <title>TrendNest</title>
 
             </Helmet>
+            <div className='py-52'>
+                <div className='flex justify-center items-center '>
+
+                    <p className="text-5xl font-extrabold py-20 text-center">{`OPP'S 404 `}<br /> PAGE NOT FOUND</p>
+                </div>
+                <div className='flex justify-center items-center'>
+                    <Link className='px-6 py-3 text-black font-macondo bg-green-400 rounded-lg text-lg font-medium' to='/'>Go Back Home </Link>
+                </div>
+            </div>
         </div>
     );
 };
